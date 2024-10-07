9ce9645bd0e5dcc861632d02ded40f2587d7b3ce
Artem Chesanovskyi <artem.chesanovskyi.ki.2022@lpnu.ua>
2024-10-07 20:03:01 +0300

    Lab - 4 (ver 1.0)

---
+++ Лаб - 4/Documentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab4","l":"Lab4()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab4","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
